c2bcd3f2ea13f95943cce8921547735f91694c4f
Pedro Akos <akos@MacBook-Air.local>
2019-07-16 16:33:08 +0100

    added some documentation over lists

--- Yggdrasil/core/data_structures/generic/list.h
+++ Yggdrasil/core/data_structures/generic/list.h
@@ -15,29 +15,69 @@
 #include "../../utils/utils.h"
 #include <stdlib.h>
 
+
 typedef struct _list_item {
-	void* data;
-	struct _list_item* next;
+	void* data; //a generic pointer to the data type stored in the list
+	struct _list_item* next; //a pointer to the next element in the list (NULL if this item is the last element)
 }list_item;
 
 typedef struct _list {
-	short size;
-	list_item* head;
-	list_item* tail;
+	short size; //number of elements in the list
+	list_item* head; //pointer to the head of the list
+	list_item* tail; //pointer to the tail of the list
 }list;
 
+/**
+ * Initialize an empty list
+ * @return a pointer to a an empty list
+ */
 list* list_init();
 
+/**
+ * Add an item to the head of the list
+ * @param l the list to add the item to
+ * @param item a generic pointer to the item to be added
+ */
 void list_add_item_to_head(list* l, void* item);
 
+/**
+ * Add an item to the tail of the list
+ * @param l the list to add the item to
+ * @param item a generic pointer to the item to be added
+ */
 void list_add_item_to_tail(list* l, void* item);
 
+/**
+ * Remove the next item pointed by previous
+ * @param l the list to remove from
+ * @param previous the pointer to the previous element
+ * @return a pointer to the removed item or NULL if no item was removed
+ */
 void* list_remove(list* l, list_item* previous);
 
+/**
+ * Remove a specific item from the list
+ * @param l the list to remove from
+ * @param equal a function to compare the items in the list in this operation (exact match boolean)
+ * @param to_remove the item to remove from the list
+ * @return a pointer to the removed item or NULL if no item was removed
+ */
 void* list_remove_item(list* l, equal_function equal, void* to_remove);
 
+/**
+ * Find a specific item in the list
+ * @param l the list
+ * @param equal a function to compare the items in the list in this operation (exact match boolean)
+ * @param to_find the item to find
+ * @return a pointer to the item found or NULL if no item was found
+ */
 void* list_find_item(list* l, equal_function equal, void* to_find);
 
+/**
+ * Remove the head of the list
+ * @param l the list to remove from
+ * @return the item that was removed or NULL if no item was removed
+ */
 void* list_remove_head(list* l);
 
 #endif /* DATA_STRUCTURES_GENERIC_LIST_H_ */
--- Yggdrasil/core/data_structures/generic/ordered_list.h
+++ Yggdrasil/core/data_structures/generic/ordered_list.h
@@ -14,37 +14,92 @@
 #include <stdlib.h>
 
 typedef struct __ordered_list_item {
-	void* data;
-	struct __ordered_list_item* next;
+	void* data; //a generic pointer to the data type stored in the list
+	struct __ordered_list_item* next; //a pointer to the next element in the list (NULL if this item is the last element)
 
-	struct __ordered_list_item* prev;
+	struct __ordered_list_item* prev; //a pointer to the previous element in the list (NULL if this item is the first element)
 }ordered_list_item;
 
 typedef struct _ordered_list {
-	short size;
-	ordered_list_item* head;
-	ordered_list_item* tail;
+	short size; //number of elements in the list
+	ordered_list_item* head; //pointer to the head of the list
+	ordered_list_item* tail; //pointer to the tail of the list
 
-	compare_function compare;
+	compare_function compare; //a function to compare the elements in the list
 }ordered_list;
 
 
+/**
+ * Initialize an empty ordered list
+ * @param compare the function used to compare (and order) the elements in the list
+ * @return a pointer to a an empty ordered list configured to order by the provided function
+ */
 ordered_list* ordered_list_init(compare_function compare);
 
+/**
+ * Add an item to the ordered list
+ * @param l the ordered list to be added to
+ * @param item the item to be added
+ */
 void ordered_list_add_item(ordered_list* l, void* item);
 
+/**
+ * Remove the item pointed by item from the ordered list
+ * @param l the ordered list to be removed from
+ * @param item the item to be removed
+ * @return the removed item data or NULL if no item was removed
+ */
 void* ordered_list_remove(ordered_list* l, ordered_list_item* item);
 
+/**
+ * Remove a specific item from the ordered list
+ * @param l the ordered list to be removed from
+ * @param equal a function to compare items in the list (exact match boolean)
+ * @param to_remove the item to be removed
+ * @return the removed item data or NULL if no item was removed
+ */
 void* ordered_list_remove_item(ordered_list* l, equal_function equal, void* to_remove);
 
+/**
+ * Find a specific item from the ordered list
+ * @param l the ordered list
+ * @param equal a function to compare items in the list (exact match boolean)
+ * @param to_find the item to be found
+ * @return the found item data or NULL if no item was found
+ */
 void* ordered_list_find_item(ordered_list* l, equal_function equal, void* to_find);
 
+/**
+ * Return the item in ordered list that has the given index
+ * @param l the ordered list
+ * @param index the index of the desired item
+ * @return the retrieved item data or NULL if no item was found
+ */
 void* ordered_list_get_item_by_index(ordered_list* l, int index);
 
+/**
+ * Remove from the head of the ordered list
+ * @param l the ordered list
+ * @return the removed item data or NULL if no item was removed
+ */
 void* ordered_list_remove_head(ordered_list* l);
 
+/**
+ * Remove from the tail of the ordered list
+ * @param l the ordered list
+ * @return the removed item data or NULL if no item was removed
+ */
 void* ordered_list_remove_tail(ordered_list* l);
 
+/**
+ * Update the item pointed by newdata in the ordered list.
+ * Newdata will be added to list, while the old data will be removed.
+ * If newdata does not exist in the ordered list the item is added to the ordered list
+ * @param l the ordered list
+ * @param equal a function to compare items in the ordered list (exact match boolean)
+ * @param newdata the data to be updated
+ * @return a pointer to the old data or NULL if newdata was not in the list.
+ */
 void* ordered_list_update_item(ordered_list* l, equal_function equal, void* newdata);
 
 #endif /* DATA_STRUCTURES_GENERIC_ORDERED_LIST_H_ */
